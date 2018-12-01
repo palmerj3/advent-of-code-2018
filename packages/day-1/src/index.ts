ea6d2c13cbd6482a1f0020cdd69ac06bbe3b9fb0
Jason Palmer <jason@jason-palmer.com>
2018-12-01 14:07:29 -0500

    Enable strict mode and fix small issue

--- packages/day-1/src/index.ts
+++ packages/day-1/src/index.ts
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const INPUT_PATH: string = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(__filename),
   "..",
   "data",
   "input"
